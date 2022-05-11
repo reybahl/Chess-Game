266812dd4e9bfee3abbda1cb768839467b0c9f97
reybahl <bahl.reyansh@gmail.com>
2022-05-11 10:26:08 -0400

    Create MyPlayground.java

---
+++ MyPlayground.java
@@ -0,0 +1,10 @@
+import org.code.playground.*;
+import org.code.media.*;
+import java.io.FileNotFoundException;
+
+public class MyPlayground {
+  public static void main(String[] args) throws FileNotFoundException, PlaygroundException {
+    ChessBoard board = new ChessBoard("chess-board.png");
+    
+  }
+}
