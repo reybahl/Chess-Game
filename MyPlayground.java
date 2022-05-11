5b9fe4799e9717e230ba50d787cc575145710bcf
Rupak Biswas <94465654+AncientBehemoth@users.noreply.github.com>
2022-05-11 16:20:05 -0400

    Update MyPlayground.java

--- MyPlayground.java
+++ MyPlayground.java
@@ -1,10 +1,8 @@
 import org.code.playground.*;
 import org.code.media.*;
 import java.io.FileNotFoundException;
-
 public class MyPlayground {
   public static void main(String[] args) throws FileNotFoundException, PlaygroundException {
-    ChessBoard board = new ChessBoard("chess-board.png");
-    
+    ChessBoard board = new ChessBoard();
   }
 }
