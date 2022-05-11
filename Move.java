7e2ac8960749cb713279a4c3716a994fb2f39ac9
reybahl <bahl.reyansh@gmail.com>
2022-05-11 17:28:41 -0400

    Fix braces

--- Move.java
+++ Move.java
@@ -2,9 +2,10 @@ import org.code.playground.*;
 import java.io.FileNotFoundException;
 public class Move extends ClickableImage {
   public Move (int xPos, int yPos) throws FileNotFoundException{
-    super("Highlight.png", xPos * 50, yPos * 50, 50, 50);
+    super("Highlight.png", yPos * 50, xPos * 50, 50, 50);
   }
   public void onClick() {
+    ;
     //Set new location for piece, process taking if applicable, move taken pieces to takenPieces array, progress turn;
   }
 }
