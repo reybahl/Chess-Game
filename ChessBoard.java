9643e853b557a3c8dce0fcccb9f8c007299040f5
Rupak Biswas <94465654+AncientBehemoth@users.noreply.github.com>
2022-05-11 16:19:36 -0400

    Update ChessBoard.java

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,64 +1,48 @@
 import org.code.playground.*;
 import java.io.FileNotFoundException;
-
 public class ChessBoard {
-  private static Piece[][] pieces = new Piece[8][8];
+  private static ClickableImage[][] squares = new ClickableImage[8][8];
+  private static Piece[][] pieces = new Piece[2][16];
   private static Piece[][] takenPieces = new Piece[2][15];
-  private static ValidMove[][] validMove = new ValidMove[8][8];
-
-  private static Board myChessBoard = Playground.board;		// the board to display the game
-  public ChessBoard (String filename) throws FileNotFoundException, PlaygroundException {
-    myChessBoard.setBackgroundImage(filename);
-    
+  public static Board myChessBoard = Playground.board;
+  public ChessBoard () throws FileNotFoundException, PlaygroundException {
     String[] pieceTypes = {"Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"};
-
-    for (int i=0; i<pieceTypes.length; i++) {
+    for (int i=0; i < 8; i++) {
       pieces[0][i] = new Piece(pieceTypes[i], "White", 0, i);
-      pieces[7][i] = new Piece(pieceTypes[i], "Black", 7, i);
+      pieces[0][8+i] = new Piece("Pawn", "White", 1, i);
+      pieces[1][i] = new Piece(pieceTypes[i], "Black", 7, i);
+      pieces[1][8+i] = new Piece("Pawn", "Black", 6, i);
     }
-    
-    for (int col=0; col<pieces[0].length; col++) {
-      pieces[1][col] = new Piece("Pawn", "White", 1, col);
-      pieces[6][col] = new Piece("Pawn", "Black", 6, col);
-    }
-    showChessBoard();
+    showPlayableBoard();
     myChessBoard.start();
-    
   }
-
-  public static boolean free (int row, int col, String color) {
-    return (pieces[row][col] == null || !pieces[row][col].getColor().equals(color));
-  }
-
-  public static boolean empty (int row, int col) {
-    return (pieces[row][col] == null);
-  }
-
-  public static Piece getPiece (int row, int col) {
-    return pieces[row][col];
-  }
-
-  public static void setPossibleMoves (int row, int col) {
-    
-    for (int r = 0; r < 8; r++) {
-      for (int c = 0; c < 8; c++) {
-        validMove[r][c] = new ValidMove(pieces[row][col].canMove(r, c), r, c);
+  public static Piece getPieceAt(int row, int col) {
+    for (int i = 0; i < 2; i++) {
+      for (int j = 0; j < 16; j++) {        
+        if (pieces[i][j].getRow() == row && pieces[i][j].getCol() == col) {
+          return pieces[i][j];
+        }
       }
     }
+    return null;
   }
-  
-  public static void showPossibleMoves (int row, int col) {
-    setPossibleMoves(row, col);
+  public static boolean empty (int row, int col) {
+    return (squares[row][col] == null);
+  }
+  public static boolean free (int row, int col, String color) {
+    return empty(row, col) || !getPieceAt(row, col).getColor().equals(color);
+  }
+  public static void showPossibleMoves (Piece piece) throws FileNotFoundException {
     for (int r=0; r<8; r++) {
       for (int c=0; c<8; c++) {
-        if (validMove[r][c] != null && validMove[r][c].getValid()) {
-          myChessBoard.addTextItem(validMove[r][c].getButton());
+        if (piece.canMove(r, c)) {
+          myChessBoard.addClickableImage(new Move(r, c));
         }
       }
     }
   }
-
-  public static void showChessBoard () throws FileNotFoundException {
+  public static void showPlayableBoard () throws FileNotFoundException {
+    myChessBoard.setBackgroundImage("Chessboard.png");
     for (Piece[] arr : pieces) {
       for (Piece piece : arr) {
         if (piece != null) {
