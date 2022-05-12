eeae212f79f890e46716e3511f274cdf0d5d1baa
reybahl <bahl.reyansh@gmail.com>
2022-05-12 09:23:49 -0400

    Piece can successfully move

--- Move.java
+++ Move.java
@@ -1,11 +1,21 @@
 import org.code.playground.*;
 import java.io.FileNotFoundException;
 public class Move extends ClickableImage {
-  public Move (int xPos, int yPos) throws FileNotFoundException{
-    super("Highlight.png", yPos * 50, xPos * 50, 50, 50);
+  private int row;
+  private int col;
+  public Move (int row, int col) throws FileNotFoundException{
+    super("Highlight.png", col * 50, row * 50, 50, 50);
+    this.row = row;
+    this.col = col;
   }
   public void onClick() {
     ChessBoard.clearPossibleMoves();
+    try {
+      ChessBoard.movePiece(this.row, this.col);    
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+    
     //Set new location for piece, process taking if applicable, move taken pieces to takenPieces array, progress turn;
   }
 }
