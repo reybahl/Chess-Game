037c27e1e72ee0880c56959aba8cf579ccb12fa1
reybahl <bahl.reyansh@gmail.com>
2022-05-12 09:55:16 -0400

    Use GameControl class in main class

--- MyPlayground.java
+++ MyPlayground.java
@@ -1,8 +1,9 @@
 import org.code.playground.*;
 import org.code.media.*;
 import java.io.FileNotFoundException;
+
 public class MyPlayground {
   public static void main(String[] args) throws FileNotFoundException, PlaygroundException {
-    ChessBoard board = new ChessBoard();
+    GameControl.start();
   }
 }
