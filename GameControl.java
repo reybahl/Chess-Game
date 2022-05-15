fcf05f7fb276f79aeb645841118bbfde28c2e1a3
Rupak Biswas <94465654+AncientBehemoth@users.noreply.github.com>
2022-05-15 14:18:21 -0400

    Update GameControl.java

--- GameControl.java
+++ GameControl.java
@@ -1,22 +1,61 @@
 import java.io.FileNotFoundException;
 import org.code.playground.*;
 import org.code.media.*;
-
 public class GameControl {
   private static ChessBoard board;
   private static String currentTurn;
-
   public static void start () throws FileNotFoundException, PlaygroundException {
     currentTurn = "White";
     board = new ChessBoard();
   }
- 
   public static String getTurn() {
     return currentTurn;
   }
-  
   public static void switchTurn() {
-    if (currentTurn.equals("White")) currentTurn = "Black";
-    else currentTurn = "White";
+    currentTurn = (currentTurn.equals("White") ? "Black" : "White");
+  }
+  public static void checkForWin() {
+    ChessBoard.setPlayerHasMove(false);
+    try {
+      for (Piece thisPiece : ChessBoard.getPiecesArray()[currentTurn.equals("White") ? 0 : 1]) {
+        ChessBoard.getPossibleMoves(thisPiece);
+      }
+      if (CheckLogic.inCheck(ChessBoard.getPiecesArray(), currentTurn) && !ChessBoard.getPlayerHasMove()) {
+        System.out.println("Checkmate! " + (currentTurn == "White" ? "Black" : "White") + " wins!");
+        ChessBoard.showTransparentOverlay();
+      } else if (!ChessBoard.getPlayerHasMove()) {
+        System.out.println("Stalemate!");
+        ChessBoard.showTransparentOverlay();
+      }
+    } catch (Exception e) {
+        System.out.println("Exception in method checkForWin() checkmate / stalemate check: " + e);
+    }
+    boolean draw = true;
+    int bishopCount = 0;
+    int knightCount = 0;
+    for (Piece[] x : ChessBoard.getPiecesArray()) {
+      for (Piece y : x) {
+        if (y.getType() != null && draw) {
+          if (y.getType().equals("Queen") || y.getType().equals("Rook") || y.getType().equals("Pawn")) {
+            draw = false;
+          } else if (y.getType().equals("Bishop")) {
+            bishopCount++;
+          } else if (y.getType().equals("Knight")) {
+            knightCount++;
+          }
+        }
+      }
+      if (bishopCount > 1  || (bishopCount == 1 && knightCount >= 1)) {
+        draw = false;
+      }
+    }
+    if (draw) {
+      try {
+        System.out.println("Draw!");
+        ChessBoard.showTransparentOverlay();
+      } catch (Exception e) {
+        System.out.println("Exception in method checkForWin() draw check: " + e);
+      }
+    }
   }
 }
