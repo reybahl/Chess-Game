ebc65ada926ba6107666aba9e2c89e48989b935c
Rupak Biswas <94465654+AncientBehemoth@users.noreply.github.com>
2022-05-15 14:15:40 -0400

    Fully Functional except repetition draw

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,13 +1,12 @@
 import org.code.playground.*;
 import java.io.FileNotFoundException;
 public class ChessBoard {
-  private static ClickableImage[][] squares = new ClickableImage[8][8];
-  private static Piece[][] pieces = new Piece[2][16];
-  private static Piece[][] takenPieces = new Piece[2][15];
   public static Board myChessBoard = Playground.board;
+  private static Piece[][] pieces = new Piece[2][16];
   private static Move[][] moves = new Move[8][8];
   private static Piece clickedPiece;
-  public ChessBoard () throws FileNotFoundException, PlaygroundException {
+  private static boolean playerHasMove;
+  public ChessBoard() throws FileNotFoundException, PlaygroundException {
     String[] pieceTypes = {"Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"};
     for (int i=0; i < 8; i++) {
       pieces[0][i] = new Piece(pieceTypes[i], "White", 0, i);
@@ -21,7 +20,13 @@ public class ChessBoard {
   public static Piece[][] getPiecesArray() {
     return pieces;
   }
-  public static Piece getPieceAt(int row, int col) {
+  public static Piece getClicked() {
+    return clickedPiece;
+  }
+  public static boolean getPlayerHasMove() {
+    return playerHasMove;
+  }
+  public static Piece getPieceAt (int row, int col) {
     for (int i = 0; i < 2; i++) {
       for (int j = 0; j < 16; j++) {        
         if (pieces[i][j].getRow() == row && pieces[i][j].getCol() == col) {
@@ -31,46 +36,41 @@ public class ChessBoard {
     }
     return null;
   }
-  public static void setClicked(Piece piece) {
+  public static void setClicked (Piece piece) {
     clickedPiece = piece;
   }
-  public static Piece getClicked() {
-    return clickedPiece;
+  public static void setPlayerHasMove (boolean x) {
+    playerHasMove = x;
   }
-  public static void movePiece (Piece[][] pieces1, int row, int col, Piece thisPiece) throws FileNotFoundException {
-    if (pieces1 == pieces) {
-      clickedPiece.move(pieces, row, col);
-      showPlayableBoard();
-    } else {
-      thisPiece.move(pieces1, row, col);
-    }
-  }
-  public static void setPieceAt(Piece[][] pieces1, int row, int col, Piece piece) {
-    for (int i = 0; i < 2; i++) {
-      for (int j = 0; j < 16; j++) {
-        if (pieces1 == pieces) {
-          if (pieces[i][j].getRow() == row && pieces[i][j].getCol() == col) {
-            pieces[i][j] = piece;
-          } else {
-            if (pieces1[i][j].getRow() == row && pieces1[i][j].getCol() == col) {
-              pieces1[i][j] = piece;
-            }
-          }
-        }
-      }
-    }
-  }
-  public static boolean empty (int row, int col) {
+  public static boolean isEmpty (int row, int col) {
     return getPieceAt(row, col) == null;
   }
-  public static boolean free (int row, int col, String color) {
-    return empty(row, col) || !getPieceAt(row, col).getColor().equals(color);
+  public static boolean isFree (int row, int col, String color) {
+    return isEmpty(row, col) || !getPieceAt(row, col).getColor().equals(color);
   }
-  public static void getPossibleMoves(Piece piece) throws FileNotFoundException {
+  public static void movePiece (int row, int col, Piece thisPiece) throws FileNotFoundException {
+    clickedPiece.move(row, col);
+    showPlayableBoard();
+  }
+  public static void removePiece (Piece piece, Piece killedBy) {
+    myChessBoard.removeClickableImage(piece.getButton());
+  }
+  public static void getPossibleMoves (Piece piece) throws FileNotFoundException {
     for (int r=0; r<8; r++) {
       for (int c=0; c<8; c++) {
         if (piece.validMove(r, c)) {
           moves[r][c] = new Move(r, c);
+          playerHasMove = true;
+        }
+      }
+    }
+  }
+  public static void showPlayableBoard() throws FileNotFoundException {
+    myChessBoard.setBackgroundImage("Chessboard.png");
+    for (Piece[] arr : pieces) {
+      for (Piece piece : arr) {
+        if (piece.getType() != null) {
+          myChessBoard.addClickableImage(piece.getButton());
         }
       }
     }
@@ -86,7 +86,7 @@ public class ChessBoard {
       }
     }
   }
-  public static void clearPossibleMoves () {
+  public static void clearPossibleMoves() {
     for (int r=0; r<8; r++) {
       for (int c=0; c<8; c++) {
         if (moves[r][c] != null) {
@@ -96,22 +96,7 @@ public class ChessBoard {
       }
     }
   }
-  public static void removePiece (Piece[][] pieces1, Piece piece, Piece killedBy) {
-    if (pieces1 == pieces) {
-      setPieceAt(pieces, piece.getRow(), piece.getCol(), killedBy);
-      myChessBoard.removeClickableImage(piece.getButton());
-    } else {
-      setPieceAt(pieces1, piece.getRow(), piece.getCol(), killedBy);
-    }
-  }
-  public static void showPlayableBoard () throws FileNotFoundException {
-    myChessBoard.setBackgroundImage("Chessboard.png");
-    for (Piece[] arr : pieces) {
-      for (Piece piece : arr) {
-        if (piece != null && piece.isAlive()) {
-          myChessBoard.addClickableImage(piece.getButton());
-        }
-      }
-    }
+  public static void showTransparentOverlay() throws FileNotFoundException {
+    myChessBoard.addImageItem(new ImageItem("Transparent.png", 0, 0, myChessBoard.getWidth(), myChessBoard.getHeight()));
   }
 }
