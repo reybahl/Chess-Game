33a501e3fd983a13302e7f38a25937dd5317e18e
Rupak Biswas <94465654+AncientBehemoth@users.noreply.github.com>
2022-05-15 14:17:41 -0400

    Update PieceButton.java

--- PieceButton.java
+++ PieceButton.java
@@ -3,27 +3,25 @@ import org.code.playground.*;
 public class PieceButton extends ClickableImage {
   int row;
   int col;
-  public PieceButton (String filename, int xPos, int yPos, int width, int height, int row, int col) throws FileNotFoundException {
+  public PieceButton (String filename, int xPos, int yPos, int width, int height, int r, int c) throws FileNotFoundException {
     super(filename, xPos, yPos, width, height);
-    this.row = row;
-    this.col = col;
+    this.row = r;
+    this.col = c;
   }
-  public void setLocation (int xPos, int yPos, int row, int col) {
+  public void setLocation (int xPos, int yPos, int r, int c) {
     super.setY(yPos);
     super.setX(xPos);
-    this.row = row;
-    this.col = col;
+    this.row = r;
+    this.col = c;
   }
   public void onClick() {
     try {
-      Piece current = ChessBoard.getPieceAt(row, col);
-      
-      if (current.getColor().equals(GameControl.getTurn())) {
-        ChessBoard.showPossibleMoves(current);
-        ChessBoard.setClicked(current);
+      if (ChessBoard.getPieceAt(row, col).getColor().equals(GameControl.getTurn())) {
+        ChessBoard.setClicked(ChessBoard.getPieceAt(row, col));
+        ChessBoard.showPossibleMoves(ChessBoard.getPieceAt(row, col));
       }
     } catch (Exception e) {
-      System.out.println(e);
+      System.out.println("Exception in method onClick() of PieceButton " + e);
     }
   }
 }
