cbdee59a2f586c4ac9fbc7a015e92a8faf6d8190
Rupak Biswas <94465654+AncientBehemoth@users.noreply.github.com>
2022-05-15 14:17:22 -0400

    Update MyPlayground.java

--- MyPlayground.java
+++ MyPlayground.java
@@ -1,7 +1,6 @@
 import org.code.playground.*;
 import org.code.media.*;
 import java.io.FileNotFoundException;
-
 public class MyPlayground {
   public static void main(String[] args) throws FileNotFoundException, PlaygroundException {
     GameControl.start();
