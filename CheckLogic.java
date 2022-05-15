43c6e36ba2b4489160ebcb0e6289ac2d28b8925e
Rupak Biswas <94465654+AncientBehemoth@users.noreply.github.com>
2022-05-15 14:16:12 -0400

    Fully functional

--- CheckLogic.java
+++ CheckLogic.java
@@ -1,11 +1,19 @@
+import java.io.FileNotFoundException;
 public class CheckLogic {
   public static Piece[][] piecesCopy = new Piece[2][16];
-  public static Piece[][] getPiecesCopy() {
+  private static void copyPiecesArray() throws FileNotFoundException {
+    for (int j = 0; j < 2; j++) {
+      for(int i = 0; i < 16; i++) {
+        piecesCopy[j][i] = new Piece (ChessBoard.getPiecesArray()[j][i].getType(), ChessBoard.getPiecesArray()[j][i].getColor(), ChessBoard.getPiecesArray()[j][i].getRow(), ChessBoard.getPiecesArray()[j][i].getCol());
+      }
+    }
+  }
+  public static Piece[][] getPiecesArray() {
     return piecesCopy;
   }
   public static Piece getPieceAt(int row, int col) {
     for (int i = 0; i < 2; i++) {
-      for (int j = 0; j < 16; j++) {        
+      for (int j = 0; j < 16; j++) {
         if (piecesCopy[i][j].getRow() == row && piecesCopy[i][j].getCol() == col) {
           return piecesCopy[i][j];
         }
@@ -13,41 +21,32 @@ public class CheckLogic {
     }
     return null;
   }
+  public static boolean isEmpty (int row, int col) {
+    return getPieceAt(row, col) == null;
+  }
+  public static boolean isFree (int row, int col, String color) {
+    return isEmpty(row, col) || !getPieceAt(row, col).getColor().equals(color);
+  }
   public static boolean inCheck (Piece[][] piecesArray, String color) {
     for (Piece aPiece : piecesArray[(color.equals("White") ? 1 : 0)]) {
-      if (aPiece.canMove(piecesArray[(color.equals("White") ? 0 : 1)][4].getRow(), piecesArray[(color.equals("White") ? 0 : 1)][4].getCol())) {
+      if (aPiece.canMove(false, piecesArray[(color.equals("White") ? 0 : 1)][4].getRow(), piecesArray[(color.equals("White") ? 0 : 1)][4].getCol())) {
         return true;
       }
     }
     return false;
   }
-  public static boolean squareUnderAttack (String color, int row, int col) {
-    for (Piece thisPiece: ChessBoard.getPiecesArray()[(color.equals("White") ? 1 : 0)]) {
-      if (thisPiece.canMove(row, col)) {
-        return true;
-      }
-    }
-    return false;
-  }
-  public static boolean noCheck (Piece piece, String color, int row, int col) {
+  public static boolean dangerResolved (Piece piece, int row, int col) {
     try {
-      for (int j = 0; j < 2; j++) {
-        for(int i = 0; i < 16; i++) {
-          Piece currentPiece = ChessBoard.getPiecesArray()[j][i];
-          piecesCopy[j][i] = new Piece (currentPiece.getType(), currentPiece.getColor(), currentPiece.getRow(), currentPiece.getCol());
-        }
+      copyPiecesArray();
+      getPiecesArray();
+      if (CheckLogic.getPieceAt(row, col) != null) {
+        CheckLogic.getPieceAt(row, col).kill(CheckLogic.getPieceAt(piece.getRow(), piece.getCol()));
+      } else {
+        CheckLogic.getPieceAt(piece.getRow(), piece.getCol()).move(row, col);
       }
-      Piece movingPiece = CheckLogic.getPieceAt(piece.getRow(), piece.getCol());
-      movingPiece.setRow(row);
-      movingPiece.setCol(col);
-      Piece there = CheckLogic.getPieceAt(row, col);
-      if (there != null) {
-        there.kill(piecesCopy, piece);
-      }
-      ChessBoard.movePiece(piecesCopy, row, col, movingPiece);
-      return !inCheck(piecesCopy, color);
+      return !inCheck(piecesCopy, piece.getColor());
     } catch (Exception e) {
-      System.out.println(e);
+      System.out.println("Exception in method dangerResolved() of CheckLogic: " + e);
     }
     return false;
   }
