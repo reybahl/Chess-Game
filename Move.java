bbbb4e32966c55e4096d2d075742b7368fe997b3
Rupak Biswas <94465654+AncientBehemoth@users.noreply.github.com>
2022-05-17 16:35:51 -0400

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,5 +1,5 @@
-import org.code.playground.*;
 import java.io.FileNotFoundException;
+import org.code.playground.*;
 public class Move extends ClickableImage {
   private int row;
   private int col;
@@ -16,26 +16,22 @@ public class Move extends ClickableImage {
         ChessBoard.getPieceAt(this.row, this.col).kill(ChessBoard.getClicked());
         ChessBoard.myChessBoard.playSound("move_sound.wav");
       }
-      ChessBoard.movePiece(this.row, this.col, null);
+      ChessBoard.moveClickedPiece(this.row, this.col);
       if (ChessBoard.getClicked().getType().equals("Pawn") && (this.row == 0 || this.row == 7)) {
         ChessBoard.getClicked().setType("Queen");
       }
-      
       GameControl.switchTurn();
-      Piece currentKing = ChessBoard.getPiecesArray()[(GameControl.getTurn().equals("White") ? 0 : 1)][4];
-      if (CheckLogic.inCheck(ChessBoard.getPiecesArray(), GameControl.getTurn())) {
-        currentKing.setImage("Red-King.png");
+      if (CheckLogic.inCheck(ChessBoard.getPiecesArray(), "White")) {
+        ChessBoard.getPiecesArray()[0][4].setImage("Red-King.png");
       } else {
-        currentKing.setType("King");
+        ChessBoard.getPiecesArray()[0][4].setType("King");
       }
-
-      Piece otherKing = ChessBoard.getPiecesArray()[(GameControl.getTurn().equals("White") ? 1 : 0)][4];
-      if (CheckLogic.inCheck(ChessBoard.getPiecesArray(), (GameControl.getTurn().equals("White") ? "Black" : "White"))) {
-        otherKing.setImage("Red-King.png");
+      if (CheckLogic.inCheck(ChessBoard.getPiecesArray(), "Black")) {
+        System.out.println("black is in check");
+        ChessBoard.getPiecesArray()[1][4].setImage("Red-King.png");
       } else {
-        otherKing.setType("King");
+        ChessBoard.getPiecesArray()[1][4].setType("King");
       }
-      
       GameControl.checkForEnd();
     } catch (Exception e) {
       System.out.println("Exception in method onClick() of Move " + e);
